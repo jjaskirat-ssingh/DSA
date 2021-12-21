030a2f94ff1cb2880f3f83c8dc3056005ef93e15
Jaskirat Singh <72308994+jjaskirat-ssingh@users.noreply.github.com>
2021-12-21 23:31:58 +0530

    Bin Sort or Bucket Sort

---
+++ Sorting/Index Based Sorting/BinOrBucketSort.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+
+int Max(int A[], int n){
+    int max = INT32_MIN;
+    for (int i=0; i<n; i++){
+        if (A[i] > max){
+            max = A[i];
+        }
+    }
+    return max;
+}
+
+// LL Node
+class Node{
+    public:
+        int value;
+        Node *next;
+};
+
+void Insert(Node **ptrBins, int idx){
+    Node *temp = new Node;
+    temp->value = idx;
+    temp->next = nullptr;
+
+    if (ptrBins[idx] == nullptr){   // ptrBins[idx] is head ptr
+        ptrBins[idx] = temp;
+    }
+    else{
+        Node *p = ptrBins[idx];
+        while (p->next != nullptr){
+            p=p->next;
+        }
+        p->next = temp;
+    }   
+}
+
+int Delete(Node **ptrBins, int idx){
+    Node *p = ptrBins[idx];     // ptrBins[idx] is head ptr
+    ptrBins[idx] = ptrBins[idx]->next;
+    int x = p->value;
+    delete p;
+    return x;
+}
+
+void BinSort(int A[], int n){
+    int max = Max(A, n);
+
+    // Create bins array
+    Node **bins = new Node *[max+1];
+
+    // Initialize bins array with nullptr
+    for (int i=0; i<max+1; i++){
+        bins[i] = nullptr;
+    }
+
+    // Update count array values based on A values
+    for (int i=0; i<n; i++){
+        Insert(bins, A[i]);
+    }
+
+    // Update A with sorted elements
+    int i = 0;
+    int j = 0;
+    while (i<max+1){
+        while (bins[i] != nullptr){
+            A[j++] = Delete(bins, i);
+        }
+        i++;
+    }
+
+    // Delete heap memory
+    delete []bins;
+}
+
+void display(int A[], int n){
+    for(int i=0; i<n; i++){
+        cout<<A[i]<<" ";
+    }
+}
+
+int main(){
+    int A[] = {2, 5, 8, 12, 3, 6, 7, 10};
+    int n = sizeof(A)/sizeof(A[0]);
+    cout<<"Before: ";
+    display(A, n);
+    cout<<endl;
+
+    BinSort(A, n);
+
+    cout<<"After: ";
+    display(A, n);
+    
+    return 0;
+}
